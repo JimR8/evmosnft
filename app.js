a86052c735cd73b32fd70b51357dc1f726adf2d3
Jim <93066619+JimR8@users.noreply.github.com>
2021-10-31 03:29:52 +1100

    Update app.js

--- app.js
+++ app.js
@@ -580,7 +580,7 @@ window.onload = () => {
           icon: "info",
         });
         const result = await response.wait();
-        let html =`<h2>You Minted #${tokenId}</h2><div><img src="/images/Evmos_AM.PNG"></div>`;
+        let html ="<h2>You Minted #" + tokenId +"</h2><div><img src='/images/Evmos_AM.PNG'></div>";
         $('div#minted').html(html);
         $.toast().reset("all");
         $.toast({
